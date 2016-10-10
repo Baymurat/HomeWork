db5edcf735379749b3c7d55c7e1331bceded9589
Baymurat <robbi92@mail.ry>
2016-10-10 17:30:35 +0400

    base

---
+++ Main.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Class contains method, which call method of class Primary.
+ * Class contains main method.
+ */
+public class Main {
+));
+    }
+}
